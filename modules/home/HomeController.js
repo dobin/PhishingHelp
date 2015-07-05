057949a041a49f701cde3d8fdd5fdc1968227b04
Dobin Rutishauser <dobin@broken.ch>
2015-07-05 13:52:55 +0200

    implement hide unregistered toggle button

--- modules/home/HomeController.js
+++ modules/home/HomeController.js
@@ -15,6 +15,7 @@ angular.module('myApp.Home', ['ngRoute'])
             $scope.domains = {};
             $scope.inputDomain = "";
             $scope.loading = false;
+            $scope.showUnregistered = true;
 
             $scope.updateTable = function() {
                 var n = 0;
@@ -23,21 +24,17 @@ angular.module('myApp.Home', ['ngRoute'])
                 for(n=0; n < $scope.domains.length; n++) {
                     if ( $scope.domains[n].isFullyResolved == false) {
                         DomainService.getPhishingDomainInfo($scope.domains[n].domain).success(function(data) {
-                            if (data.isFullyResolved == true) {
-                                // TODO FIXME FUCKING N CHANGES
-                                for(var i=0; i<$scope.domains.length; i++) {
-                                    if ($scope.domains[i].domain == data.domain) {
+                            // TODO FIXME FUCKING N CHANGES
+                            for(var i=0; i<$scope.domains.length; i++) {
+                                if ($scope.domains[i].domain == data.domain) {
+                                    if (! angular.equals($scope.domains[i], data)) {
                                         $scope.domains[i] = data;
-                                        console.log("Resolve: " + i + " : " + data.domain);
                                     }
-
                                 }
                             }
                         });
                     }
                 }
-
-                console.log("blerg");
             }
 
             $interval( function() { $scope.updateTable(); }, 10000 );
@@ -57,39 +54,12 @@ angular.module('myApp.Home', ['ngRoute'])
                     $scope.loading = false;
                 });
             }
+
+            $scope.toggleShowUnregistered = function() {
+                $scope.showUnregistered = ! $scope.showUnregistered;
+
+            }
         }
     ])
-    /*
-    .directive('myCurrentTime', ['$interval', 'dateFilter',
-        function($interval, dateFilter) {
-            // return the directive link function. (compile function not needed)
-            return function(scope, element, attrs) {
-                var format,  // date format
-                    stopTime; // so that we can cancel the time updates
-
-                // used to update the UI
-                function updateTime() {
-                    element.text(dateFilter(new Date(), format));
-                }
-
-                // watch the expression, and update the UI on change.
-                scope.$watch(attrs.myCurrentTime, function(value) {
-                    format = value;
-                    updateTime();
-                });
-
-                stopTime = $interval(updateTime, 1000);
-
-                // listen on DOM destroy (removal) event, and cancel the next UI update
-                // to prevent updating time after the DOM element was removed.
-                element.on('$destroy', function() {
-                    $interval.cancel(stopTime);
-                });
-            }
-        }])
-*/
-;
-
-
 ;
 
